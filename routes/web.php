174c1a294fd6e8e7f37fd9e93fbe8004abe22d95
elisha-mustapha <mustygambo1999@gmail.com>
2024-07-02 11:30:34 +0100

    new project set up

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', [AppController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
